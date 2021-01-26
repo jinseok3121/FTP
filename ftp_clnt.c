d9c3d6a0d15294b78b00887f8c24ccbd8abd80bf
OJS <ohjinseok3121@kookmin.ac.kr>
2021-01-26 17:40:52 +0900

    initial version

---
+++ ftp_clnt.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/rand.h>
+#include <openssl/pem.h>
+#include <assert.h>
+
+#include "readnwrite.h"
+#include "enc.h"
+#include "msg.h"
+
+#define BUF_SIZE 1024
+#define NAME_SIZE 20
+
+void error_handling(char*msg);
+void* send_msg(void* arg);
+void* recv_msg(void* arg);
+
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	struct sockaddr_in serv_addr;
+	pthread_t snd_thread;
+	void * thread_return;
+	
+	if(argc!=3)
+	{
+		printf("Usage : %s <IP> <port> \n", argv[0]);
+		exit(1);
+	} 
+	
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("connect() error");
+	
+	
+	printf("----------------------------------Welcome FTP----------------------------------\n");
+	printf("Write commend (signup or login)\n\n\n");
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_join(snd_thread, &thread_return);	
+	close(sock);
+
+	return 0;
+}
+
+void* send_msg(void* arg)
+{
+	int sock = *((int*)arg);
+	char msg[BUF_SIZE];
+	
+	while(1)
+	{
+		printf("commend : ");
+		fgets(msg, BUF_SIZE, stdin);
+		if(!strcmp(msg, "Q\n") || !strcmp(msg, "q\n"))
+		{
+			close(sock);
+			exit(0);
+		}
+		
+		else if(!strcmp(msg, "SIGNUP\n") || !strcmp(msg, "signup\n"))
+		{
+			sprintf(msg, "%s", msg);
+			write(sock, msg, strlen(msg));
+      
+			printf("Input your ID : ");
+			fgets(msg, 30, stdin);
+			write(sock, msg, strlen(msg));
+			
+			printf("Input your PW : ");
+			fgets(msg, 30, stdin);
+			write(sock, msg, strlen(msg));
+		
+			continue;
+		}
+
+		else if(!strcmp(msg, "LOGIN\n") || !strcmp(msg, "login\n"))
+		{
+			sprintf(msg, "%s", msg);
+			write(sock, msg, strlen(msg));
+      
+			printf("Input your ID : ");
+			fgets(msg, 20, stdin);
+			write(sock, msg, strlen(msg));
+			
+			printf("Input your PW : ");
+			fgets(msg, 20, stdin);
+			write(sock, msg, strlen(msg));
+			
+			sleep(1);
+			read(sock, msg, sizeof(msg));
+			printf("%s\n", msg);
+			
+			memset(msg, 0, BUF_SIZE);
+			
+			continue;	
+		}
+		
+		else if(!strcmp(msg, "LIST\n") || !strcmp(msg, "list\n"))
+		{
+			sprintf(msg, "%s", msg);
+			write(sock, msg, strlen(msg));
+			
+			memset(msg, 0, BUF_SIZE);
+			read(sock, msg, sizeof(msg));
+			printf("%s\n", msg);
+			memset(msg, 0, BUF_SIZE);
+			continue;
+		}
+		
+		else if(!strcmp(msg, "DOWN\n") || !strcmp(msg, "down\n"))
+		{
+			char filename[20];
+			int fd;
+			int datalen;
+			int str_len;
+			sprintf(msg, "%s", msg);
+			write(sock, msg, strlen(msg));
+			
+			printf("Input downlowad file name : ");
+			fgets(msg, 20, stdin);
+			msg[strnlen(msg, sizeof(msg))-1] = '\0';
+			printf("Input save file name: ");
+			fgets(filename, 20, stdin);
+			filename[strnlen(filename, sizeof(filename))-1] = '\0';
+			write(sock, msg, strlen(msg));
+			sleep(1);
+			
+			
+			fd = open(filename, O_CREAT | O_WRONLY, S_IRWXU);
+			if(fd != -1)
+				printf("File open success\n");
+			
+			read(sock, &datalen, sizeof(int));
+			datalen = ntohl(datalen);
+			
+				
+			while((datalen -(str_len = read(sock, msg, sizeof(msg))) >0))
+			{
+				write(fd, msg, str_len);
+				if(str_len < BUF_SIZE)
+				{
+					break;
+				}
+			}
+			close(fd);
+			
+			sleep(1);
+			memset(msg, 0 ,BUF_SIZE);
+			printf("Download Success!!!!\n");
+			continue;
+		
+		}
+		
+		else if(!strcmp(msg, "UP\n") || !strcmp(msg, "up\n"))
+		{
+			int fd;
+			int file_len;
+			int read_len;
+			char filename[20];
+			
+			sprintf(msg, "%s", msg);
+			write(sock, msg, strlen(msg));
+		
+			printf("Input upload file name : ");
+			fgets(msg, 20, stdin);
+			msg[strnlen(msg, sizeof(msg))-1] = '\0';
+			printf("Input save file name : ");
+			fgets(filename, 20, stdin);
+			write(sock, filename, strlen(filename));
+			
+			sleep(1);
+			
+			filename[strnlen(filename, sizeof(filename))-1] = '\0';
+			
+			fd = open(msg, O_RDONLY, S_IRWXU);
+			
+			file_len = lseek(fd, 0, SEEK_END);
+			file_len = htonl(file_len);
+			write(sock, &file_len, sizeof(file_len));
+	
+			lseek(fd, 0, SEEK_SET);
+				
+			while((read_len = read(fd, msg, sizeof(msg))) > 0)
+			{
+				//printf("reading file\n");
+				if(write(sock, msg, read_len) !=  read_len)
+				{
+					error_handling("write() error");
+				}
+			}
+			
+		
+			sleep(1);
+			memset(msg, 0, BUF_SIZE);
+			
+			printf("Upload Success!!!!\n");
+			continue;
+		}
+		
+	
+		
+	}
+	return NULL;	
+}
+
+void error_handling(char *msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
---
+++ ftp_serv.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/rand.h>
+#include <openssl/pem.h>
+#include <assert.h>
+
+#include "readnwrite.h"
+#include "enc.h"
+#include "msg.h"
+
+#define BUF_SIZE 1024
+#define MAX_CLNT 256
+
+void * hanlde_clnt(void * arg);
+void error_handling(char*msg);
+
+int clnt_cnt =0;
+int clnt_socks[MAX_CLNT];
+char ID[MAX_CLNT][30] = {0x00, };  
+char PW[MAX_CLNT][30] ={0x00, };
+int cnt = 0;
+pthread_mutex_t mutex;
+
+int main(int argc, char * argv[])
+{
+	int cnt_i;
+	int serv_sock;
+	int clnt_sock;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_size;
+	pthread_t t_id;
+	//
+	APP_MSG msg_in;
+	APP_MSG msg_out;
+	
+	char plaintext[BUFSIZE + AES_BLOCK_SIZE] = {0x00, };
+	int n;
+	int len;
+	int plaintext_len;
+	int ciphertext_len;
+	int publickey_len;
+	int encryptedkey_len;
+	
+	unsigned char key[AES_KEY_128] = {0x00, };
+	unsigned char iv[AES_KEY_128] = {0x00, };
+	unsigned char buffer[BUFSIZE] = {0x00, };
+	
+	BIO *pb_public = NULL, *pb_private = NULL;
+	BIO *pub = NULL;
+	RSA *rsa_pubkey = NULL, *rsa_privkey = NULL;
+	
+	for(cnt_i = 0; cnt_i < AES_KEY_128; cnt_i++)
+	{
+		iv[cnt_i] = (unsigned char)cnt_i;
+	}
+	
+	//
+	
+	
+	if(argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+	
+	pthread_mutex_init(&mutex, NULL);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+	
+	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+	{
+		error_handling("bind() error");
+	}
+	
+	if(listen(serv_sock, 5) == -1)
+	{
+		error_handling("listen() error");
+	}
+	// key generation
+	pb_public = BIO_new_file("public.pem", "r");
+	PEM_read_bio_RSAPublicKey(pb_public, &rsa_pubkey, NULL, NULL);
+	
+	pb_private = BIO_new_file("private.pem", "r");
+	PEM_read_bio_RSAPrivateKey(pb_private, &rsa_privkey, NULL, NULL);
+	
+	while(1)
+	{
+		clnt_addr_size = sizeof(clnt_addr_size);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+		
+		pthread_mutex_lock(&mutex);
+		clnt_socks[clnt_cnt++] = clnt_sock;
+		pthread_mutex_unlock(&mutex);
+	
+		pthread_create(&t_id, NULL, hanlde_clnt, (void*)&clnt_sock);
+		pthread_detach(t_id);
+		printf("\n[TCP Server] Client connected: IP=%s, port=%d\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+		
+	}
+	
+	close(serv_sock);
+	return 0;
+}
+
+
+void * hanlde_clnt(void *arg)
+{
+	int clnt_sock = *((int*)arg);
+	int str_len = 0, i;   
+	char msg[BUF_SIZE];
+	
+	
+	while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) 
+	{
+		if(!strcmp(msg, "SIGNUP\n") || !strcmp(msg, "signup\n"))
+		{
+			
+			str_len = read(clnt_sock, msg, sizeof(msg));
+			msg[str_len] = '\0';
+			pthread_mutex_lock(&mutex);
+			memcpy(&ID[cnt], msg, strlen(msg));
+			pthread_mutex_unlock(&mutex); 
+			printf("NEW ID : %s\n", ID[cnt]);
+			
+			str_len = read(clnt_sock, msg, sizeof(msg));
+			msg[str_len] = '\0';
+			pthread_mutex_lock(&mutex);
+			memcpy(&PW[cnt], msg, strlen(msg));
+			pthread_mutex_unlock(&mutex);
+			printf("NEW PW : %s\n", PW[cnt]);
+			
+			cnt++;
+			memset(msg, 0 ,BUF_SIZE);
+			
+			continue;
+		}
+		
+		else if(!strcmp(msg, "LOGIN\n") || !strcmp(msg, "login\n"))
+		{
+			char CKID[30];
+			char CKPW[30];
+			int checkID = 1;
+			int checkPW = 1;
+			int str_len;
+			char err[] = {"Try again login\n"};
+			printf("Client login.....\n");
+			
+			str_len = read(clnt_sock, CKID, sizeof(CKID));
+			CKID[str_len] = '\0';
+			printf("ID : %s\n", CKID);
+			str_len = read(clnt_sock, CKPW, sizeof(CKPW));
+			CKPW[str_len] = '\0';
+			printf("PW : %s\n", CKPW);
+			
+			for(int i = 0; i<MAX_CLNT; i++)
+			{
+				checkID = strcmp(ID[i] , CKID);
+				checkPW = strcmp(PW[i] , CKPW);
+				
+				//printf("ID : %d\n", checkID);
+				//printf("PW : %d\n", checkPW);
+
+				if(checkID == 0  && checkPW == 0)
+				{
+					char com[] = {"write commend(list or down or up)\n"};
+					write(clnt_sock, com, sizeof(com));
+					memset(msg, 0 ,BUF_SIZE);
+					break;
+				}
+				else
+				{
+					continue;
+					memset(msg, 0 ,BUF_SIZE);
+				}
+			}
+			write(clnt_sock, err, sizeof(err));	
+			memset(msg, 0 ,BUF_SIZE);
+			continue;
+		}
+		
+		else if(!strcmp(msg, "LIST\n") || !strcmp(msg, "list\n"))
+		{
+			printf("list.....\n");
+		
+			int fd;
+			int read_len = 0;
+			fd = open("directory.txt", O_RDONLY, S_IRWXU);
+			
+			while((read_len = read(fd, msg, sizeof(msg))) >0)
+			{
+				write(clnt_sock, msg, read_len);	
+			}
+			memset(msg, 0 ,BUF_SIZE);
+			continue;
+		}
+		
+		else if(!strcmp(msg, "DOWN\n") || !strcmp(msg, "down\n"))
+		{
+			printf("Download.....\n");
+			str_len = read(clnt_sock, msg, sizeof(msg));
+			
+			int fd;
+			int read_len = 0;
+			int file_len;
+			fd = open(msg, O_RDONLY, S_IRWXU);
+			
+			file_len = lseek(fd, 0, SEEK_END);
+			file_len = htonl(file_len);
+			write(clnt_sock, &file_len, sizeof(file_len));
+	
+			lseek(fd, 0, SEEK_SET);
+				
+			while((read_len = read(fd, msg, sizeof(msg))) > 0)
+			{
+				//printf("reading file\n");
+				if(write(clnt_sock, msg, read_len) !=  read_len)
+				{
+					error_handling("write() error");
+				}
+			}
+			
+			sleep(1);
+
+			memset(msg, 0 ,BUF_SIZE);	
+			
+			
+		}
+		
+		else if(!strcmp(msg, "UP\n") || !strcmp(msg, "up\n"))
+		{
+			int fd;
+			int fd2;
+			char *filename;
+			int datalen;
+			
+			printf("Upload.....\n");
+			str_len = read(clnt_sock, msg, sizeof(msg));
+			msg[str_len] = '\0';
+			printf("file name :%s", msg);
+			filename = msg;
+			
+			fd2 = open("directory.txt", O_APPEND | O_WRONLY, S_IRWXU);
+			write(fd2, filename, strlen(filename));
+			close(fd2);
+			sleep(1);
+
+			fd = open(filename, O_CREAT | O_WRONLY, S_IRWXU);
+			if(fd != -1)
+				printf("File open success\n");
+				
+			read(clnt_sock, &datalen, sizeof(int));
+			datalen = ntohl(datalen);
+			printf("datalen : %d\n", datalen);
+			
+			while((datalen -(str_len = read(clnt_sock, msg, sizeof(msg))) >0))
+			{
+				write(fd, msg, str_len);
+				if(str_len < BUF_SIZE)
+				{
+					break;
+				}
+			}
+			lseek(fd, 0, SEEK_SET);
+			close(fd);
+			
+			memset(msg, 0 ,BUF_SIZE);
+			continue;
+		
+		}			
+	}
+	
+	pthread_mutex_lock(&mutex);
+	for(i = 0; i<clnt_cnt; i++)
+	{
+		if(clnt_sock == clnt_socks[i])
+		{
+			while(i++<clnt_cnt - 1)
+				clnt_socks[i] = clnt_socks[i+1];
+			break;
+		}
+	}
+	clnt_cnt--;
+	pthread_mutex_unlock(&mutex);
+	close(clnt_sock);
+	
+}
+
+void error_handling(char *msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
